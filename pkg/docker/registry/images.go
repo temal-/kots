e404344ae8ab3753e7d1af00237e563aa4c78faf
divolgin <dmitriy@replicated.com>
2020-05-22 17:25:33 +0000

    Support proxying public images

--- pkg/docker/registry/images.go
+++ pkg/docker/registry/images.go
@@ -1,16 +1,22 @@
 package registry
 
 import (
+	"fmt"
 	"strings"
 )
 
 func MakeProxiedImageURL(proxyHost string, appSlug string, image string) string {
+	fmt.Printf("+++++convert %s ->\n", image)
 	parts := strings.Split(image, "@")
 	if len(parts) == 2 {
-		return strings.Join([]string{proxyHost, "proxy", appSlug, parts[0]}, "/")
+		ttt := strings.Join([]string{proxyHost, "proxy", appSlug, parts[0]}, "/")
+		fmt.Printf("+++++to 1 %s\n", ttt)
+		return ttt
 	}
 
 	// TODO: host with a port breaks this
 	parts = strings.Split(image, ":")
-	return strings.Join([]string{proxyHost, "proxy", appSlug, parts[0]}, "/")
+	ttt := strings.Join([]string{proxyHost, "proxy", appSlug, parts[0]}, "/")
+	fmt.Printf("+++++to 2 %s\n", ttt)
+	return ttt
 }
